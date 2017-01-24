297501f03ae2fd5351d887486c5985857fe73f6b
RyTaus <rtaus1@lion.lmu.edu>
2017-01-24 14:50:54 -0800

    improved orginization

---
+++ turing-machine/visual_scripting/turing-visual-scripting.js
@@ -0,0 +1,6 @@
+"use strict"
+
+var Tape = require('./../tape.js');
+var TuringMachine = require('./../turing-machine.js').TuringMachine;
+var State = require('./../turing-machine.js').State;
+var Edge = require('./../turing-machine.js').Edge;
