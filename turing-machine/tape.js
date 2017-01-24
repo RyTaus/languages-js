83f8a8b689b1379909e9e88ccd3d7daa34181ba5
RyTaus <rtaus1@lion.lmu.edu>
2017-01-24 12:00:13 -0800

    improved orginization

--- turing-machine/tape.js
+++ turing-machine/tape.js
@@ -1,42 +1,42 @@
 "use strict"
 
 class Tape {
-  constructor (str) {
-      this.cells = str.split('');
-      this.pointer = 0;
-  }
-
-  moveRight() {
-    this.pointer += 1;
-    if (this.pointer > (this.cells).length) {
-      this.cells.push('#');
+    constructor(str) {
+        this.cells = str.split('');
+        this.pointer = 0;
     }
-    return this;
 
-  }
+    moveRight() {
+        this.pointer += 1;
+        if (this.pointer > (this.cells).length) {
+            this.cells.push('#');
+        }
+        return this;
 
-  moveLeft() {
-    this.pointer -=1;
-    if (this.pointer < 0) {
-      this.pointer = 0;
     }
-    return this;
 
-  }
+    moveLeft() {
+        this.pointer -= 1;
+        if (this.pointer < 0) {
+            this.pointer = 0;
+        }
+        return this;
 
-  readCell() {
-    return this.cells[this.pointer];
-  }
+    }
 
-  writeCell(char) {
-    this.cells[this.pointer] = char;
-    return this;
-  }
+    readCell() {
+        return this.cells[this.pointer];
+    }
 
-  toString() {
-    let str = this.cells.join("");
-    return str.slice(0, this.pointer) + "." + str.slice(this.pointer);
-  }
+    writeCell(char) {
+        this.cells[this.pointer] = char;
+        return this;
+    }
+
+    toString() {
+        let str = this.cells.join("");
+        return str.slice(0, this.pointer) + "." + str.slice(this.pointer);
+    }
 }
 
 module.exports = Tape;
